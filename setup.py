819c97668e6b35204b161ba3c3df833b11353d66
Jamal <jamal.zeynalov@gmail.com>
2024-07-09 12:09:19 +0300

    Upgrade requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pytest-jira-xfail",
-    version="1.2.0",
+    version="1.2.1",
     author="Jamal Zeinalov",
     author_email="jamal.zeynalov@gmail.com",
     description="Plugin skips (xfail) tests if unresolved Jira issue(s) linked",
@@ -27,7 +27,7 @@ setup(
         "allure-pytest>=2.11.1",
         "selenium>=4.6.0",
         "pytest-playwright>=0.3.3",
-        "playwright~=1.43.0",
+        "playwright>=1.43.0",
     ],
     python_requires=">=3.9",
 )
